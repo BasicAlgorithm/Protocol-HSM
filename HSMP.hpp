e8dc2ae5ccb8b16e2171c0241d74031ec7efb3f3
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-04-12 22:26:07 -0500

    Add HSMP structures declaration

---
+++ HSMP.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class HSMP {
+  struct Login {};
+
+  struct Lista {};
+
+  struct Message {};
+
+  struct Broadcast {};
+
+  struct UploadFile {};
+
+  struct File_AN {};
+
+  struct Exit {};
+
+  struct Error {};
+};
